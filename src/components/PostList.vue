09195cee9eb20b4d2876cd649c0bb25f901c2524
zhanwei <zhangwei@duduyixia.com>
2022-09-12 16:19:38 +0800

    init

---
+++ src/components/PostList.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="post-list">
+    <signle-post v-for="post in posts" :key="post.id" :post="post">
+
+    </signle-post>
+  </div>
+</template>
+
+<script>
+import SinglePost from "@/components/SinglePost";
+export default {
+  components: { SinglePost },
+  props: ["posts"]
+};
+</script>
+<style></style>
