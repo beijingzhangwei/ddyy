b979b9385e74e3adf1915341e91dc218d81092d4
zhanwei <zhangwei@duduyixia.com>
2022-09-12 16:19:34 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <the-application-bar></the-application-bar>
+    <router-view class="content" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TheApplicationBar from "./components/UI/TheApplicationBar.vue";
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    TheApplicationBar
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+.content {
+  padding-top: 5.5vh;
+  padding-left: 10%;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
-import { createApp } from 'vue'
+// import { createApp } from 'vue'
+// import App from './App.vue'
+//
+// createApp(App).mount('#app')
+import Vue from 'vue'
 import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
+import store from './store'
 
-createApp(App).mount('#app')
+Vue.config.productionTip = false
+
+new Vue({
+    router,
+    store,
+    render: h => h(App)
+}).$mount('#app')
