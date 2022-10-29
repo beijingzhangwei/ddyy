b23651fb32a7ccdf768b781f1b4b07cdadfe8141
zhanwei <zhangwei@duduyixia.com>
2022-10-29 23:54:31 +0800

    init

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="container">
-    <h3>用户信息</h3>
+    <h3>用户信息 {{user}}</h3>
     <!--    user 本页面的计算函数-->
-    <h3>用户名:{{ user.email }}</h3>
+    <h3>用户名:{{ user.email}}</h3>
     <h3>用户描述:{{ user.description }}</h3>
     <post-list :posts="userPosts" title="最近发帖："></post-list>
   </div>
@@ -12,7 +12,7 @@ import PostList from "@/components/PostList";
 export default {
   components: { PostList },
   props: {
-    email: { type: String, default: "" } // 这里是什么时候初始化的呢？ 列表参数？
+    email: { type: String, default: "" }, // 这里是什么时候初始化的呢？ 列表参数？
   },
   computed: {
     userPosts() {
@@ -21,8 +21,30 @@ export default {
     },
     user() {
       // console.log("-----###组件mounted时###-----user_id=" + this.$store.getters["auth/currentUser"].user_id);
-      // console.log("-----###组件computed时（有缓存）-user###email==" + this.$store.getters["users/getUser"](this.email));
+      // const xx =  this.userFetch(this.email)
+      // console.log("----xxxxxxxxxxxxxxxxxxxxxxxxl==" + xx.email);
       return this.$store.getters["auth/currentUser"]
+      // return xx;
+    }
+  },
+  methods: {
+    async userFetch(email) {
+      console.log("----xxxxxxxxxxxxxxxxxxxxxxxxl==" + email);
+      const u = await fetch("https://ddyydy.tk/ddyy-b/api/user_by_email/" + email, {
+        method: "GET"
+      })
+          .then(response => {
+            if (!response.ok) throw new Error("Cannot get user");
+            return response.json();
+          })
+          .catch(error => {
+            console.log(error);
+            throw error;
+          });
+      await u
+      console.log("----qqqqqqqqqqqyyyyyyyyyyyyy==" + u);
+      console.log("----qqqqqqqqqqqyyyyyyyyyyyyy==" + u.email);
+      return u;
     }
   },
   mounted() {
